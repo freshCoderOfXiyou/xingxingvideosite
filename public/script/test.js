0cd952a9fc527326c1c3af0bf82ba79e41adeaf9
江杨阳 <18829291427@163.com>
2017-04-20 16:29:36 +0800

    Update test.js

--- public/script/test.js
+++ public/script/test.js
@@ -3,11 +3,13 @@
 	window.onload=function () {
 		var video = document.getElementsByTagName("video")[0]
 		var cav = document.getElementsByTagName("canvas")[0]
+		//设置常量canvas的高度以及宽度
 		var cavWidth = 800 
 		var cavHeight = 420
 		cav.width=cavWidth
 		cav.height=cavHeight 
 		var ctx = cav.getContext("2d")
+		//存储弹幕对象的数组
 		var capObjs = []
 		var lastItemTime
 		var capHeight = 20 
@@ -15,11 +17,14 @@
 		var sendEle = document.getElementsByClassName("caption-sendButton")[0]
 		var colorUl = document.getElementsByClassName("colorItems")[0]
 		var ismoveInputEle = document.getElementsByClassName("caption-input-ismove")[0]
+		//弹幕颜色
 		var colors=["#fff","#FFCCCC","#CCFFCC","#CCCCFF","#FFFFCC","#CCFFFF"]
 		var selectedColorIndex = 0
 		var prevPlayTime = 0
+		//测试数据的数组
 		var testArrayCopy = []
 		var capobjId = 0
+		//弹幕在画布中高度可能值组成的数组
 		var topObjs = [{blank:true , value : 20 ,index:0},
 						{blank:true , value : 50 ,index:1},
 						{blank:true , value : 80 ,index:2},
@@ -34,7 +39,7 @@
 						{blank:true , value : 350 ,index:11},
 						{blank:true , value : 380 ,index:12},
 						{blank:true , value : 410 ,index:13}]
-//test data
+//test data 测试数据
 var testArray = [{content:"ABCDEFGHIJKLMNOPQRSTUVWXYZ",time:"1",ismove:false,colorIndex:0},
 {content:"233333333333333",time:"2",ismove:true,colorIndex:0},
 {content:"干杯，哈哈哈~~~~~~",time:"2",ismove:true,colorIndex:5},
@@ -87,8 +92,10 @@ var testArray = [{content:"ABCDEFGHIJKLMNOPQRSTUVWXYZ",time:"1",ismove:false,col
 {content:"马上就下课了，瓦罗蓝大陆走起了～～～",time:"35",ismove:true,colorIndex:5},
 {content:"马上就下课了，瓦罗蓝大陆走起了～～～",time:"36",ismove:true,colorIndex:2},
 {content:"马上就下课了，瓦罗蓝大陆走起了～～～",time:"37",ismove:true,colorIndex:2}]
-
+		//将测试数据备份
 		copyArray(testArray , testArrayCopy)
+		/*弹幕对象的构造函数，参数分别是：1.ismove：弹幕是否是移动的弹幕,2.spe:弹幕的移动速度，3.col：弹幕的颜色，4.text：弹幕的文本*/
+		/*原型链方法 setTopValue设置纵坐标，setLeftValue设置横坐标，moving完成坐标的改变，setId完成id值的设置*/
 		function Caption( ismove , spe , col , text ) {
 			this.isMove = ismove
 			this.speed = spe
@@ -147,7 +154,8 @@ var testArray = [{content:"ABCDEFGHIJKLMNOPQRSTUVWXYZ",time:"1",ismove:false,col
 		var cap1 = new Caption(  false , 1 , 0 , "小礼物走一波，双击6666。。。。")
 		capObjs.push(cap1)
 		cap1.setId()
-
+		
+		//循环遍历数组，根据对象的属性绘制在画布上
 		function drawAllText () {
 			ctx.clearRect( 0 , 0 , cavWidth , cavHeight)
 			ctx.beginPath()
@@ -164,7 +172,8 @@ var testArray = [{content:"ABCDEFGHIJKLMNOPQRSTUVWXYZ",time:"1",ismove:false,col
 				// }
 			}
 		}
-
+		
+		//更新数组，当对象已经超出范围的时候从数组删除这个对象
 		function refreshObjs(objs) {
 			for (var i = objs.length - 1; i >= 0; i--) {
 				if (objs[i].left < - cavWidth || objs[i].latestTime > 450 ) {
@@ -173,12 +182,14 @@ var testArray = [{content:"ABCDEFGHIJKLMNOPQRSTUVWXYZ",time:"1",ismove:false,col
 
 			}
 		}
-
+		
+		//更新保存弹幕对象的数组
 		function updateArray () {
 			var now = parseInt( video.currentTime )
 			for (var i = testArray.length - 1; i >= 0; i--) {
 				var nowItemTime = parseInt(testArray[i].time) 
 				if ( nowItemTime == now ) {
+					//首次写的控制高度的方式，空间利用不充分，后来改为setTopValue中的方式
 					// var nowItemLeft = getLeftValue(testArray[i])
 					// var diffTime = Math.abs(nowItemTime - lastItemTime)
 					// if (diffTime < 6) { 
@@ -194,7 +205,8 @@ var testArray = [{content:"ABCDEFGHIJKLMNOPQRSTUVWXYZ",time:"1",ismove:false,col
 				}
 			}
 		}
-
+		
+		//当用户点击send发送弹幕的回调函数
 		function sendCaption (argument) {
 			var inputEleTxt = inputEle.value
 			var now = parseInt( video.currentTime )
@@ -214,7 +226,8 @@ var testArray = [{content:"ABCDEFGHIJKLMNOPQRSTUVWXYZ",time:"1",ismove:false,col
 		// 		return nowItemLeft
 		// 	}
 		// }
-
+		
+		//重新启动canvas，用在人为导致进度条时间的改变
 		function reinitCav (argument) {
 			// testArray = testArrayCopy
 			copyArray(testArrayCopy , testArray)
@@ -226,7 +239,8 @@ var testArray = [{content:"ABCDEFGHIJKLMNOPQRSTUVWXYZ",time:"1",ismove:false,col
 		}
 
 		var canvasTimer = null 
-
+		
+		//初始化canvas，用在开始播放时
  		function initCanvas () {
  			if (canvasTimer == null ) {
 				canvasTimer = setInterval(function (argument) {
@@ -237,14 +251,15 @@ var testArray = [{content:"ABCDEFGHIJKLMNOPQRSTUVWXYZ",time:"1",ismove:false,col
  			}
 			
 		}//end function initCanvas
-
+		
+		//复制数组
 		function copyArray (arr1 , arr2) {
 			for (var i =0 , len=arr1.length ; i < len ; i++) {
 					arr2[i] = arr1[i]
 				}	
 		}
 
-		//color select event
+		//color select event 用户发送弹幕的颜色控制代码
 		colorUl.addEventListener("click", function( e ){
 			var prevSelectItemId = ""
 			switch (selectedColorIndex) {
@@ -303,7 +318,8 @@ var testArray = [{content:"ABCDEFGHIJKLMNOPQRSTUVWXYZ",time:"1",ismove:false,col
 		video.addEventListener("playing" , function () {
 			initCanvas()
 		})
-
+		
+		//进度条改变执行代码
 		video.addEventListener("timeupdate", function  () {
 			var nowPlayTime = video.currentTime
 			var diffTime = Math.abs(nowPlayTime - prevPlayTime)
@@ -312,14 +328,17 @@ var testArray = [{content:"ABCDEFGHIJKLMNOPQRSTUVWXYZ",time:"1",ismove:false,col
 				reinitCav()
 			}
 		}, false)
-
+		
+		//视频暂停执行代码
 		video.addEventListener("pause" , function () {
 			clearInterval(canvasTimer)
 			canvasTimer = null 
 		})
-
+		
+		//点击send的监听事件
 		sendEle.addEventListener("click" , sendCaption)
-
+		
+		//input的回车监听事件
 		inputEle.addEventListener("keydown", function(e) {
 			var keynum = 0
 			keynum = window.event ? e.keyCode : e.which
